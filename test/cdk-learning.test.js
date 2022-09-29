8d9d2664d5183031297a382ef1307f399c4d9357
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2022-09-29 10:34:27 +0200

    Initial commit

---
+++ test/cdk-learning.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CdkLearning = require('../lib/cdk-learning-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-learning-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkLearning.CdkLearningStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
