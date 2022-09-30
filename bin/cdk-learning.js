6d5c2bbfb3f3a499b61b8db337bbac0ff1cc2f5a
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2022-09-30 16:27:42 +0200

    test lambda with s3

--- bin/cdk-learning.js
+++ bin/cdk-learning.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
-
-const cdk = require('aws-cdk-lib');
-const { CdkLearningStack } = require('../lib/cdk-learning-stack');
+const cdk = require("aws-cdk-lib");
+const { CdkLearningStack } = require("../lib/cdk-learning-stack");
 
 const app = new cdk.App();
-new CdkLearningStack(app, 'CdkLearningStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const cdkLearningStack = new CdkLearningStack(
+  app,
+  "aws-node-http-api-project-dev",
+  {
+    env: { account: "909852739473", region: "us-east-1" },
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  }
+);
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// Add a tag to all constructs in the stack
+cdk.Tags.of(cdkLearningStack).add("s1", "labweek");
