e854d23a19651bc50ba5b76dbf80c3b635f9c057
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2022-10-05 13:51:48 +0100

    source maps

--- src/lambda.js
+++ src/lambda.js
@@ -7,14 +7,20 @@ const bucketName = process.env.BUCKET;
 exports.main = async function (event, context) {
   const res = _.chunk(['a', 'b', 'c', 'd'], 2);
   console.log('res', res)
+   console.log("BUCKET NAME", process.env.BUCKET)
   
    throw new Error("Example error here")
+  
+  
   
   try {
     console.log("Entering Lambda!");
     console.log("event", JSON.stringify(event));
     console.log("context", JSON.stringify(context));
     const data = await S3.listObjectsV2({ Bucket: bucketName }).promise();
+    
+   
+    
     
     
 
